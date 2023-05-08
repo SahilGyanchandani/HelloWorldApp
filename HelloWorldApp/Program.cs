e49d26a73d854aa1eb078bd405703340d04d576d
Sahil Gyanchandani <gyanchandanisahil8@gmail.com>
2023-05-08 15:57:06 +0530

    Second Commit

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace HelloWorldApp
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HelloWorldApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Program pg= new Program();
+            pg.Sum(10, 20);
+            //Console.WriteLine("Enter The First Value:");
+            //string val1=Console.ReadLine();
+            //Console.WriteLine("Enter the Second Value:");
+            //string val2=Console.ReadLine();
+
+            //int no1 = Convert.ToInt32(val1);
+            //int no2=Convert.ToInt32(val2);
+
+            //int result=pg.Sum(no1,no2);
+            //Console.WriteLine(result); 
+
+            
+        }
+        public int Sum(int no1,int no2)
+        {
+            int add = 0;
+            add = no1 + no2;
+            Console.WriteLine(add);
+            return add;
+
         }
     }
 }
