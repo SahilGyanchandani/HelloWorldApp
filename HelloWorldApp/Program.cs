ca3cdd2e33a45e50e06ee1ebf55455f58c3d8d05
Sahil Gyanchandani <gyanchandanisahil8@gmail.com>
2023-05-08 16:04:26 +0530

    Third Commit

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -8,25 +8,27 @@ namespace HelloWorldApp
         {
             Console.WriteLine("Hello, World!");
             Program pg= new Program();
-            pg.Sum(10, 20);
-            //Console.WriteLine("Enter The First Value:");
-            //string val1=Console.ReadLine();
-            //Console.WriteLine("Enter the Second Value:");
-            //string val2=Console.ReadLine();
-
-            //int no1 = Convert.ToInt32(val1);
-            //int no2=Convert.ToInt32(val2);
-
-            //int result=pg.Sum(no1,no2);
-            //Console.WriteLine(result); 
+            //pg.Sum(10, 20);
+            Console.WriteLine("Enter The First Value:");
+            string val1 = Console.ReadLine();
+            Console.WriteLine("Enter the Second Value:");
+            string val2 = Console.ReadLine();
 
             
+            
+            int no1 = Convert.ToInt32(val1);
+            int no2 = Convert.ToInt32(val2);
+
+            int result = pg.Sum(no1, no2);
+            Console.WriteLine(result);
+
+
         }
         public int Sum(int no1,int no2)
         {
             int add = 0;
             add = no1 + no2;
-            Console.WriteLine(add);
+            //Console.WriteLine(add);
             return add;
 
         }
